91cd440189f778859a9657f4d1e410655498a284
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2020-03-14 15:24:20 +0100

    add more functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,34 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+})
+
+// update username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // update name via chatroom  
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset the form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMssg.innerText = '', 3000);
+})
 
 // class instancess
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'shaun');
+const chatroom = new Chatroom('general', localStorage.username || 'anon');
 
 // get chats and render
 chatroom.getChats(data => chatUI.render(data));
